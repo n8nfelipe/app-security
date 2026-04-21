f3216a64ff8c58e80e558d739576abe5c9a20a41
N8nFelipe <n8nfelipe@gmail.com>
2026-04-21 14:14:23 -0300

    minor - mock mais robusto

--- app/backend/tests/conftest.py
+++ app/backend/tests/conftest.py
@@ -4,5 +4,5 @@ from unittest.mock import patch, MagicMock
 
 @pytest.fixture(autouse=True)
 def mock_init_db():
-    with patch("app.db.session.init_db"):
+    with patch("app.db.session.init_db"), patch("app.db.session.engine"), patch("app.db.session.SessionLocal"):
         yield
